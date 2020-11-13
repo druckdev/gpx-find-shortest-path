07673c9995a4c60bf8ca1b55a9483b1039e4687b
druckdev <63563978+druckdev@users.noreply.github.com>
2020-11-13 02:31:08 +0100

    Rewrite dup_nodes.py and comment the new version
    Use a dictionary with the points coordinates as keys to find shared
    points between routes. This drops the runtime complexity from O(n^2)
    down to O(n) and makes numpy unnecessary.

--- gpx_route_find_dup_nodes.py
+++ gpx_route_find_dup_nodes.py
@@ -2,14 +2,13 @@
 # https://janakiev.com/blog/gps-points-distance-python/
 # https://pypi.org/project/gpxpy/
 
-# Dependencies: numpy, geopy, gpxpy
+# Dependencies: geopy, gpxpy
 # Install all with "pip install <package>"
 
 import sys
 from typing import Optional
 
 from gpxpy import parse as gpx_parse
-import numpy as np
 
 import utils
 
@@ -21,53 +20,54 @@ def main(file_name: Optional[str] = None):
         gpx_file = open(sys.argv[1], 'r')
     else:
         input("No input file found.\n")
-        sys.exit(1)
+        return 1
 
     gpx = gpx_parse(gpx_file)
 
-    numRoutes = len(gpx.routes)
-    foundStarts = np.zeros(numRoutes, dtype=bool)
-    foundEnds = np.zeros(numRoutes, dtype=bool)
+    # Build a dictionary with coordinates (as tuple) as keys and a list of
+    # tuples as values. These lists contain all possible route and their point
+    # indices for a point with the according coordinates.
+    names = []
+    points = {}
+    for i in range(len(gpx.routes)):
+        names.append(gpx.routes[i].name)
+        for j in [0, -1]:
+            p = utils.point_to_pos(gpx.routes[i].points[j])
+            if p in points:
+                points[p].append((i, j))
+            else:
+                points[p] = [(i, j)]
 
-    for i in range(numRoutes):
-        start1 = gpx.routes[i].points[0]
-        end1 = gpx.routes[i].points[-1]
-        for j in range(i + 1, numRoutes):
-            start2 = gpx.routes[j].points[0]
-            end2 = gpx.routes[j].points[-1]
+    # Print the gpx files routes in a somewhat readable format, listing all
+    # start and end points as well as if they are shared points of different
+    # routes.
+    lookup = ["starts", "ends"]
+    for p in points.values():
+        if len(p) == 1:
+            route, point = p[0]
+            print(names[route], lookup[point], "at ", end='')
+            print(gpx.routes[route].points[point].name)
+            continue
+        for i in range(len(p) - 1):
+            r1, p1 = p[i]
+            r2, p2 = p[i + 1]
+            print(names[r1], lookup[p1], end='')
+            print(" where ", end='')
+            print(names[r2], lookup[p2], end='')
+            print(" (", gpx.routes[r1].points[p1].name, ")", sep='')
 
-            if utils.pos_equal(start1, start2):
-                print(gpx.routes[i].name + " (" + start1.name + ") and " + gpx.routes[j].name + " (" + start2.name + ") have the same start point")
-                foundStarts[i] = True
-                foundStarts[j] = True
-            elif utils.pos_equal(start1, end2):
-                print(gpx.routes[i].name + " (", start1.name + ") starts where " + gpx.routes[j].name + " (" + end2.name + ") ends")
-                foundStarts[i] = True
-                foundEnds[j] = True
-            if utils.pos_equal(end1, start2):
-                print(gpx.routes[i].name + " (" + end1.name + ") ends where " + gpx.routes[j].name + " (" + start2.name + ") starts")
-                foundEnds[i] = True
-                foundStarts[j] = True
-            elif utils.pos_equal(end1, end2):
-                print(gpx.routes[i].name + " (" + end1.name + ") and " + gpx.routes[j].name + " (" + end2.name + ") have the same end point")
-                foundEnds[i] = True
-                foundEnds[j] = True
-
-        if not foundStarts[i]:
-            print(gpx.routes[i].name + " (" + start1.name + ") starting point is alone")
-        if not foundEnds[i]:
-            print(gpx.routes[i].name + " (" + end1.name + ") end point is alone")
+    # Additionally, print the routes lengths.
     print()
-
+    print("Lengths: ")
     for route in gpx.routes:
         length = utils.route_length(route)
         print(route.name, ":", length, "km")
 
-    if sys.argv != ['']:
-        input("Press ENTER to exit.\n")
+    if file_name is not None:
+        return 1
     else:
-        return gpx
+        input("Press ENTER to exit.\n")
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
